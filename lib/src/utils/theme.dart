44cac1c4cf40e27a850f0a8915f54dddc88ecb51
Santo Shakil <shakil.mezbah@gmail.com>
2023-05-18 20:26:10 +0600

    theme

--- lib/src/utils/theme.dart
+++ lib/src/utils/theme.dart
@@ -4,9 +4,9 @@ ThemeData get defaultDarkTheme => ThemeData(
       useMaterial3: true,
       brightness: Brightness.dark,
       colorSchemeSeed: Colors.teal,
-      listTileTheme: const ListTileThemeData(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-      ),
+      cardTheme: const CardTheme(shape: circular20Shape),
+      listTileTheme: const ListTileThemeData(shape: circular20Shape),
     );
+
+const circular20Shape = RoundedRectangleBorder(borderRadius: circular20Border);
+const circular20Border = BorderRadius.all(Radius.circular(20));
