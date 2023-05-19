f08204e1299a6b2b9a3d9f1884d8f0c47ce81785
Santo Shakil <shakil.mezbah@gmail.com>
2023-05-19 18:21:43 +0600

    FFI

--- lib/src/modules/ffi/provider/ffi.p.dart
+++ lib/src/modules/ffi/provider/ffi.p.dart
@@ -1,14 +1,17 @@
 import 'dart:ffi';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_frenzy_multi_threading/src/modules/components/code/provider/code.p.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 
 const binaryPath = 'assets/bin/librust_dir_watcher.dylib';
+
+final receivePort = ReceivePort();
 
 typedef HashPathFFI = Pointer<Utf8> Function(
   Pointer<Utf8> sourcePath,
@@ -16,6 +19,8 @@ typedef HashPathFFI = Pointer<Utf8> Function(
 );
 
 void ffiFuntion(Ref ref) async {
+  ref.read(executionRunningProvider.notifier).state = true;
+  ref.read(executionMessageProvider.notifier).state = 'Hashing files...';
   final picker = FilePicker.platform;
   final sourcePath = await picker.getDirectoryPath();
   final destinationPath = await picker.getDirectoryPath();
@@ -26,20 +31,28 @@ void ffiFuntion(Ref ref) async {
   final binary = binaryByteData.buffer.asUint8List();
   await binaryFile.writeAsBytes(binary);
 
-  final ffi = DynamicLibrary.open(binaryFile.path);
+  final isolate = await Isolate.spawn(
+    (v) {
+      final ffi = DynamicLibrary.open(v.$1);
 
-  final hashPathFFI = ffi.lookupFunction<HashPathFFI, HashPathFFI>('hash_path_ffi');
+      final hashPathFFI = ffi.lookupFunction<HashPathFFI, HashPathFFI>('hash_path_ffi');
 
-  final sourcePathPointer = sourcePath.toNativeUtf8();
-  final destinationPathPointer = destinationPath.toNativeUtf8();
+      final sourcePathPointer = sourcePath.toNativeUtf8();
+      final destinationPathPointer = destinationPath.toNativeUtf8();
 
-  final result = hashPathFFI(sourcePathPointer, destinationPathPointer);
+      final result = hashPathFFI(sourcePathPointer, destinationPathPointer);
 
-  final message = result.toDartString();
+      v.$2.send(result.toDartString());
 
-  debugPrint(message);
+      calloc.free(sourcePathPointer);
+      calloc.free(destinationPathPointer);
+      calloc.free(result);
+    },
+    (binaryFile.path, receivePort.sendPort),
+  );
 
-  calloc.free(sourcePathPointer);
-  calloc.free(destinationPathPointer);
-  calloc.free(result);
+  final message = await receivePort.first as String;
+  ref.read(executionMessageProvider.notifier).state = message;
+  ref.read(executionRunningProvider.notifier).state = false;
+  isolate.kill();
 }
