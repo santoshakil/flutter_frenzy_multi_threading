99c2bb987363bef60557a1afe2e8f0aede36bf4f
Santo Shakil <shakil.mezbah@gmail.com>
2023-05-19 17:35:10 +0600

    WIP: FFI

--- lib/src/modules/ffi/provider/ffi.p.dart
+++ lib/src/modules/ffi/provider/ffi.p.dart
@@ -1,1 +1,47 @@
-void ffiFuntion() {}
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
+
+const binaryPath = 'assets/bin/dir_watch_mac';
+
+typedef HashPathFFI = Pointer<Utf8> Function(
+  Pointer<Utf8> sourcePath,
+  Pointer<Utf8> destinationPath,
+);
+
+void ffiFuntion(Ref ref) async {
+  final picker = FilePicker.platform;
+  final sourcePath = await picker.getDirectoryPath();
+  final destinationPath = await picker.getDirectoryPath();
+  if (sourcePath == null || destinationPath == null) return;
+
+  final binaryFile = File('${(await getApplicationDocumentsDirectory()).path}/dir_watch_mac');
+  await binaryFile.delete();
+  await binaryFile.create(recursive: true);
+  final binaryByteData = await rootBundle.load(binaryPath);
+  final binary = binaryByteData.buffer.asUint8List();
+  await binaryFile.writeAsBytes(binary);
+
+  final ffi = DynamicLibrary.open(binaryFile.path);
+
+  final hashPathFFI = ffi.lookupFunction<HashPathFFI, HashPathFFI>('hash_path_ffi');
+
+  final sourcePathPointer = sourcePath.toNativeUtf8();
+  final destinationPathPointer = destinationPath.toNativeUtf8();
+
+  final result = hashPathFFI(sourcePathPointer, destinationPathPointer);
+
+  final message = result.toDartString();
+
+  debugPrint(message);
+
+  calloc.free(sourcePathPointer);
+  calloc.free(destinationPathPointer);
+  calloc.free(result);
+}
