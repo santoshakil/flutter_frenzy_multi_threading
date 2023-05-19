d84b745093a1bf5e0e7bade9dad42ff32da21aff
Santo Shakil <shakil.mezbah@gmail.com>
2023-05-19 15:49:48 +0600

    Synchronization

--- lib/src/modules/synchronization/provider/synchronization.p.dart
+++ lib/src/modules/synchronization/provider/synchronization.p.dart
@@ -1,3 +1,24 @@
-void withoutSynchronization() {}
+import 'package:flutter/foundation.dart';
+import 'package:flutter_frenzy_multi_threading/src/modules/components/code/provider/code.p.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:synchronized/synchronized.dart';
 
-void wihtSynchronization() {}
+final _lock = Lock();
+
+void withoutSynchronization(Ref ref) async {
+  ref.read(executionMessageProvider.notifier).state = 'Executing without synchronization: ${DateTime.now()}';
+  await compute(_task, null);
+  ref.read(executionMessageProvider.notifier).state = 'Executed without synchronization: ${DateTime.now()}';
+  await Future.delayed(const Duration(seconds: 1));
+}
+
+void wihtSynchronization(Ref ref) async {
+  await _lock.synchronized(() async {
+    ref.read(executionMessageProvider.notifier).state = 'Executing with synchronization: ${DateTime.now()}';
+    await compute(_task, null);
+    ref.read(executionMessageProvider.notifier).state = 'Executed with synchronization: ${DateTime.now()}';
+    await Future.delayed(const Duration(seconds: 1));
+  });
+}
+
+Future<void> _task(_) async => await Future.delayed(const Duration(seconds: 2));
