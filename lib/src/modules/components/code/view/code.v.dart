79802293c5b4e7839a982f7910c5f899e7a276cf
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-05-19 23:58:11 +0600

    image zoom functionality

--- lib/src/modules/components/code/view/code.v.dart
+++ lib/src/modules/components/code/view/code.v.dart
@@ -29,7 +29,8 @@ class CodePreview extends ConsumerWidget {
                 if (code.isNotEmpty)
                   Container(
                     alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                     child: Text(
                       code,
                       style: context.text.titleLarge!.copyWith(
@@ -38,13 +39,7 @@ class CodePreview extends ConsumerWidget {
                     ),
                   ),
                 Expanded(
-                  child: Image.asset(
-                    'assets/images/${(code.isNotEmpty ? code : module.title).snackCase}.png',
-                    filterQuality: FilterQuality.high,
-                    key: ValueKey('CodePreview$code'),
-                    fit: BoxFit.contain,
-                    isAntiAlias: true,
-                  ),
+                  child: ImageWithZoom(code: code, module: module),
                 ),
               ],
             );
@@ -52,5 +47,71 @@ class CodePreview extends ConsumerWidget {
         ),
       ),
     );
+  }
+}
+
+class ImageWithZoom extends StatefulWidget {
+  const ImageWithZoom({
+    super.key,
+    required this.code,
+    required this.module,
+  });
+
+  final String code;
+  final Modules module;
+
+  @override
+  State<ImageWithZoom> createState() => _ImageWithZoomState();
+}
+
+class _ImageWithZoomState extends State<ImageWithZoom>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  Animation<Matrix4>? _animation;
+  late TransformationController _transformationController;
+  final double _minScale = 1.0;
+  final double _maxScale = 14.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return InteractiveViewer(
+      transformationController: _transformationController,
+      onInteractionEnd: (d) => resetStartAnimation(),
+      clipBehavior: Clip.none,
+      minScale: _minScale,
+      maxScale: _maxScale,
+      child: Image.asset(
+        'assets/images/${(widget.code.isNotEmpty ? widget.code : widget.module.title).snackCase}.png',
+        filterQuality: FilterQuality.high,
+        key: ValueKey('CodePreview${widget.code}'),
+        fit: BoxFit.contain,
+        isAntiAlias: true,
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 300))
+      ..addListener(() {
+        _transformationController.value = _animation!.value;
+      });
+    _transformationController = TransformationController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+    _transformationController.dispose();
+  }
+
+  void resetStartAnimation() {
+    _animation = Matrix4Tween(
+            begin: _transformationController.value, end: Matrix4.identity())
+        .animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.fastOutSlowIn));
   }
 }
